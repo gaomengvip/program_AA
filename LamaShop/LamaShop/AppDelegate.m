e8120fb07d035d30bf78d340942c2ed6038c981c
lanou3g <lanou3g@lanou3gdeMac-mini-5.local>
2015-10-06 15:18:45 +0800

    [updata]  开始写代码

--- LamaShop/LamaShop/AppDelegate.m
+++ LamaShop/LamaShop/AppDelegate.m
@@ -20,6 +20,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    NSLog(@"开始写代码");
     return YES;
 }
 
